d859ce0640368b83a8a1508a834b4d4f48034fe9
jade-walker2 <wal17042@byui.edu>
2023-02-16 10:31:44 -0700

    new changes

---
+++ chamber/scripts/chamber-script.js
@@ -0,0 +1,28 @@
+var date = document.lastModified;
+document.getElementById("result").innerHTML = "The document was last modified on : " + date;
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("date").innerHTML = year;
+
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+  }

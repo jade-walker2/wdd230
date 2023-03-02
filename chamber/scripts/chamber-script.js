69d7a79fd7adb5a421099f4e064b17df2abdc677
jade-walker2 <wal17042@byui.edu>
2023-03-01 19:34:28 -0700

    new changes

--- chamber/scripts/chamber-script.js
+++ chamber/scripts/chamber-script.js
@@ -19,10 +19,6 @@ datefield.innerHTML = `<em>${fulldate}</em>`;
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-	var x = document.getElementById("myLinks");
-	if (x.style.display === "block") {
-	  x.style.display = "none";
-	} else {
-	  x.style.display = "block";
-	}
+	document.querySelector(".menu").classList.toggle("active");
   }
+  document.querySelector(".icon").addEventListener("click",myFunction);

005543374eb487cb1253dfebe9acf9625be3bd07
jade-walker2 <wal17042@byui.edu>
2023-03-07 11:11:31 -0700

    new changes

--- lesson-10/scripts/weather.js
+++ lesson-10/scripts/weather.js
@@ -1,33 +1,3 @@
-var date = document.lastModified;
-document.getElementById("result").innerHTML = "The document was last modified on : " + date;
-
-const d = new Date();
-let year = d.getFullYear();
-document.getElementById("date").innerHTML = year;
-
-// select the elements to manipulate (output to)
-const datefield = document.querySelector(".date");
-
-// derive the current date using a date object
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-// long, medium, short options ... try them
-
-datefield.innerHTML = `<em>${fulldate}</em>`;
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-	document.querySelector(".menu").classList.toggle("active");
-  }
-  document.querySelector(".icon").addEventListener("click",myFunction);
-
-  if (now.getDay()===1 || now.getDay()===2){
-      document.querySelector("#notification").style.display="block"
-  }
-
-// WEATHER
 // select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');

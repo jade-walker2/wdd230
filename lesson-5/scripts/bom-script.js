306be3dcd2b35baad96634793ef8fd2c6d571ba8
jade-walker2 <wal17042@byui.edu>
2023-03-13 19:57:31 -0600

    new changes

--- lesson-5/scripts/bom-script.js
+++ lesson-5/scripts/bom-script.js
@@ -1,1 +1,22 @@
-const input
+
+const button = document.querySelector("#submit");
+const list = document.querySelector("#list");
+
+button.addEventListener('click', function() {
+    let del = document.createElement("button");
+    del.textContent = "X";
+    let li = document.createElement("li");
+    const input = document.querySelector("#favchap").value;
+    if (input != ""){
+        li.appendChild(document.createTextNode(input));
+        li.appendChild(del);
+        list.appendChild(li);
+        document.querySelector("#favchap").value = "";
+    }
+    else {
+        alert("This is empty please type something.");
+    }
+    del.addEventListener("click", ()=> {
+        list.removeChild(li);
+    })
+});
